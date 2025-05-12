5b0980f42848c34b173e38da77b0ad45511b1043
Jagulan <gsjagulan@gmail.com>
2025-05-12 16:12:06 +1000

    Create Sign up/Sign in page and connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:chat_app/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_app/firebase_options.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const App());
 }
 
@@ -17,7 +24,7 @@ class App extends StatelessWidget{
           seedColor: const Color.fromARGB(255, 63, 17, 177)
         ),
       ),
-      home: ...
+      home: const AuthScreen()
     );
   }
 }
